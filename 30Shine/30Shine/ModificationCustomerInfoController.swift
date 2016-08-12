e6d0d962148d0553a2f48be4b079c6ce5f8df1cd
nguyensonvu1102 <vunsse03669@fpt.edu.vn>
2016-08-12 11:15:05 +0700

    UI Modification Customer

---
+++ 30Shine/30Shine/ModificationCustomerInfoController.swift
@@ -0,0 +1,19 @@
+//
+//  ModificationCustomerInfoController.swift
+//  30Shine
+//
+//  Created by Apple on 8/12/16.
+//  Copyright © 2016 vu. All rights reserved.
+//
+
+import UIKit
+
+class ModificationCustomerInfoController: UIViewController {
+
+    @IBOutlet weak var btnUpdate: UIButton!
+    @IBOutlet weak var txtYob: UITextField!
+    @IBOutlet weak var txtMob: UITextField!
+    @IBOutlet weak var txtDob: UITextField!
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtPhone: UITextField!
+}
