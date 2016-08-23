1731cfb479394f29f1e71fed30e794cafbc3f5c8
Đỗ Ngọc Trình <trinh191195@gmail.com>
2016-08-23 11:51:11 +0700

    +offline advise skin

--- 30Shine/30Shine/AdviseSkinView.swift
+++ 30Shine/30Shine/AdviseSkinView.swift
@@ -10,6 +10,7 @@ import RxCocoa
 import RxSwift
 import Alamofire
 import RealmSwift
+import ReachabilitySwift
 
 class AdviseSkinView: UIView {
     
@@ -19,6 +20,8 @@ class AdviseSkinView: UIView {
     
     var advise : Variable<[AdviseHair]> = Variable([])
     var product : Variable<[Product]> = Variable([])
+    var reachability : Reachability?
+    var isConnectInternet = true
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -57,30 +60,77 @@ class AdviseSkinView: UIView {
             row,data,cell in
             cell.lblTitle.text = data.productName
             cell.lblPrice.text = "\(data.price)"
-            LazyImage.showForImageView(cell.imvAvatar, url: data.thumb, completion: {
-                
-            })
+            //            LazyImage.showForImageView(cell.imvAvatar, url: data.thumb, completion: {
+            //
+            //            })
+            self.showAndDownloadImage(cell.imvAvatar, url: data.thumb, imageName: data.thumb)
         }
     }
     
     //MARK: config data
     func configData() {
-        let cId = Login.getLogin().id
-        self.parseJson(cId) {
-            // have data
-            if self.advise.value[0].dateConsultant != "" {
-                self.configTableView()
-                self.bindingData()
-            }
-                // not have data
-            else {
-                self.addImage()
+        do {
+            reachability = try! Reachability.reachabilityForInternetConnection()
+        }
+        reachability!.whenReachable = {
+            reachability in
+            self.isConnectInternet = true
+            dispatch_async(dispatch_get_main_queue()) {
+                self.advise.value = []
+                let cId = Login.getLogin().id
+                self.parseJson(cId) {
+                    // have data
+                    if self.advise.value[0].dateConsultant != "" {
+                        self.bindingData()
+                    }
+                        // not have data
+                    else {
+                        self.addImage()
+                    }
+                }
             }
         }
+        reachability!.whenUnreachable = {
+            reachability in
+            self.isConnectInternet = false
+            dispatch_async(dispatch_get_main_queue()) {
+                self.advise.value = []
+                self.product.value = []
+                if AdviseHair.getAdviseHair() != nil {
+                    self.advise.value.append(AdviseHair.getAdviseHair())
+                    // have data
+                    if self.advise.value[0].dateConsultant != "" {
+                        self.bindingData()
+                    }
+                        // not have data
+                    else {
+                        self.addImage()
+                    }
+                }
+                if Product.getAllProduct().count != 0 {
+                    self.product.value = Product.getAllProduct()
+                }
+            }
+        }
+        
+        self.configTableView()
+        
+        try! reachability?.startNotifier()
     }
     
     func bindingData() {
         self.lblNote.text = self.advise.value[0].descriptionn
+    }
+    
+    func checkData() {
+        // have data
+        if self.advise.value[0].dateConsultant != "" {
+            self.bindingData()
+        }
+            // not have data
+        else {
+            self.addImage()
+        }
     }
     
     func parseJson(customerId : Int, completion : () -> ()) {
@@ -119,4 +169,45 @@ class AdviseSkinView: UIView {
             }
         }
     }
+    
+    //MARK: Helper
+    func alert(title : String, msg : String) {
+        let alert = UIAlertView(title: title, message: msg, delegate: nil, cancelButtonTitle: "Xác nhận")
+        alert.show()
+    }
+}
+
+extension AdviseSkinView {
+    func showAndDownloadImage(imageView: UIImageView, url: String, imageName : String) {
+        if self.isConnectInternet {
+            LazyImage.showForImageView(imageView, url: url, defaultImage: IMG_DEFAULT, completion: {
+                dispatch_async(dispatch_get_global_queue(0, 0), {
+                    let newName = imageName.stringByReplacingOccurrencesOfString("/", withString: "")
+                    if let dataa = UIImageJPEGRepresentation(imageView.image!, 0.8) {
+                        let filename = self.getDocumentsDirectory().stringByAppendingPathComponent(newName)
+                        dataa.writeToFile(filename, atomically: true)
+                    }
+                    
+                })
+            })
+        }
+        else {
+            imageView.image = UIImage(contentsOfFile: self.getImagePathFromDisk(imageName))
+        }
+    }
+    
+    func getImagePathFromDisk(name : String) -> String {
+        let newName = name.stringByReplacingOccurrencesOfString("/", withString: "")
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        let getImagePath = paths.stringByAppendingString("/\(newName)")
+        //self.imv.image = UIImage(contentsOfFile: getImagePath)
+        return getImagePath
+    }
+    
+    func getDocumentsDirectory() -> NSString {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
 }
