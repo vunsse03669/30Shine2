93cc107f71863cc6a7ed910885120dfbfa9eca50
nguyensonvu1102 <vunsse03669@fpt.edu.vn>
2016-08-27 20:24:12 +0700

    .......

---
+++ 30Shine/30Shine/ForgetPasswordController.swift
@@ -0,0 +1,18 @@
+//
+//  ForgetPasswordController.swift
+//  30Shine
+//
+//  Created by Apple on 8/27/16.
+//  Copyright © 2016 vu. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ForgetPasswordController: UIViewController {
+    @IBOutlet weak var btnSubmit: UIButton!
+    @IBOutlet weak var txtMail: UITextField!
+    @IBOutlet weak var txtPhone: UITextField!
+
+}
