5e6c84f8508e99147f8f2d77f5b3d75c32504e55
nguyensonvu1102 <vunsse03669@fpt.edu.vn>
2016-08-29 21:02:58 +0700

    ...

--- 30Shine/30Shine/AppDelegate.swift
+++ 30Shine/30Shine/AppDelegate.swift
@@ -203,8 +203,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        FIRMessaging.messaging().disconnect()
-        print("Disconnected from FCM.")
+//        FIRMessaging.messaging().disconnect()
+//        print("Disconnected from FCM.")
     }
     
     func applicationWillEnterForeground(application: UIApplication) {
@@ -214,6 +214,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         connectToFcm()
+        application.applicationIconBadgeNumber = 0
     }
     
     func applicationWillTerminate(application: UIApplication) {
